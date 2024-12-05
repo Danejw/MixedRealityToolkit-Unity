5b729dc36bacf4c3df6e5eb3b25376a0f2c059d4
Danejw <48073085+Danejw@users.noreply.github.com>
2024-12-04 16:08:43 -1000

    bypassed the auth flow for now

--- UnityProjects/MRTKDevTemplate_clone_0/Assets/Dane/UIManager.cs
+++ UnityProjects/MRTKDevTemplate_clone_0/Assets/Dane/UIManager.cs
@@ -12,11 +12,14 @@ namespace ClearView
 
         private void Start()
         {
-            SetAuthState(false);
+            // TODO: set back to false at start
+            SetAuthState(true);
 
             // Sets the auth state based on the auth events
-            App.Instance.MicrosoftAuth.OnAuthenticated += (string token) => { SetAuthState(true); };
-            App.Instance.MicrosoftAuth.OnSignOut += () => { SetAuthState(false); };
+
+            // Bypass auth for now
+            //App.Instance.MicrosoftAuth.OnAuthenticated += (string token) => { SetAuthState(true); };
+            //App.Instance.MicrosoftAuth.OnSignOut += () => { SetAuthState(false); };
         }
 
         private void SetAuthState(bool state)
